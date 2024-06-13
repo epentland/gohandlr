5552ee3ee51a2af16c0db6467003796e0ef9e38e
Eugene Pentland <epentland@2yfv.com>
2024-06-13 23:18:06 +0000

    project restructure

---
+++ examples/hello_world/main.go
@@ -0,0 +1,18 @@
+package main
+
+import (
+	"fmt"
+	"net/http"
+
+	"github.com/go-chi/chi/v5"
+	"github.com/go-chi/chi/v5/middleware"
+)
+
+func main() {
+	r := chi.NewMux()
+	r.Use(middleware.Logger)
+	err := http.ListenAndServe(":8084", r)
+	if err != nil {
+		fmt.Println(err)
+	}
+}
